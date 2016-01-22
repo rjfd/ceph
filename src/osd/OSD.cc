530d59cf920bbc75cc46b8e550c8885aa6c3c26f
Samuel Just <sjust@redhat.com>
2016-01-21 18:39:56 -0800

    osd/: Use MOSDPGUpdateLogMissing to implement mark_unfound_lost_delete safely
    Using a MOSDPGLog was unsafe since it is not ordered with
    respect to repops.  Instead, use a new message sent through
    the same paths as repops.
    
    Signed-off-by: Samuel Just <sjust@redhat.com>

--- src/osd/OSD.cc
+++ src/osd/OSD.cc
@@ -5213,7 +5213,9 @@ void OSD::do_command(Connection *con, ceph_tid_t tid, vector<string>& cmd, buffe
 	  // simulate pg <pgid> cmd= for pg->do-command
 	  if (prefix != "pg")
 	    cmd_putval(cct, cmdmap, "cmd", prefix);
-	  r = pg->do_command(cmdmap, ss, data, odata);
+	  r = pg->do_command(cmdmap, ss, data, odata, con, tid);
+	  if (r == -EAGAIN)
+	    goto out_skip_reply;
 	} else {
 	  ss << "not primary for pgid " << pgid;
 
@@ -5513,6 +5515,7 @@ void OSD::do_command(Connection *con, ceph_tid_t tid, vector<string>& cmd, buffe
     reply->set_data(odata);
     con->send_message(reply);
   }
+ out_skip_reply:
   return;
 }
 
