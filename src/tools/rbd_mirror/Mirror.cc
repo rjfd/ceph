880f7b023ba42b79db630a1bd127669c7716b240
Ricardo Dias <rdias@suse.com>
2016-03-15 16:39:50 +0000

    rbd: rbd-mirroring: start replayers asynchronously
    Signed-off-by: Ricardo Dias <rdias@suse.com>

--- src/tools/rbd_mirror/Mirror.cc
+++ src/tools/rbd_mirror/Mirror.cc
@@ -30,7 +30,8 @@ namespace mirror {
 Mirror::Mirror(CephContext *cct) :
   m_cct(cct),
   m_lock("rbd::mirror::Mirror"),
-  m_local(new librados::Rados())
+  m_local(new librados::Rados()),
+  m_replayers_lock("rbd::mirror::Mirror::replayers_map")
 {
   cct->lookup_or_create_singleton_object<Threads>(m_threads,
                                                   "rbd_mirror::threads");
@@ -80,15 +81,23 @@ void Mirror::update_replayers(const map<peer_t, set<int64_t> > &peer_configs)
   assert(m_lock.is_locked());
   for (auto &kv : peer_configs) {
     const peer_t &peer = kv.first;
-    if (m_replayers.find(peer) == m_replayers.end()) {
+    m_replayers_lock.Lock();
+    auto it = m_replayers.find(peer);
+    m_replayers_lock.Unlock();
+    if (it == m_replayers.end()) {
       dout(20) << "starting replayer for " << peer << dendl;
-      unique_ptr<Replayer> replayer(new Replayer(m_threads, m_local, peer));
-      // TODO: make async, and retry connecting within replayer
-      int r = replayer->init();
-      if (r < 0) {
-	continue;
-      }
-      m_replayers.insert(std::make_pair(peer, std::move(replayer)));
+      Replayer *replayer = new Replayer(m_threads, m_local, peer);
+      FunctionContext *ctx = new FunctionContext(
+          [this, peer, replayer](int r) {
+            if (r == 0) {
+              dout(20) << "insert new replayer for " << peer << dendl;
+              m_replayers_lock.Lock();
+              m_replayers.insert(std::make_pair(
+                    peer, unique_ptr<Replayer>(replayer)));
+              m_replayers_lock.Unlock();
+            }
+          });
+      replayer->init(ctx);
     }
   }
 
