6e622a3876cdc3a3f08c7070d4f25fd24e14fd5b
Ricardo Dias <rdias@suse.com>
2018-12-13 15:41:16 +0000

    mgr/dashboard: access_control: change CLI commands declaration
    Signed-off-by: Ricardo Dias <rdias@suse.com>

--- src/pybind/mgr/dashboard/tests/__init__.py
+++ src/pybind/mgr/dashboard/tests/__init__.py
@@ -3,6 +3,9 @@ from __future__ import absolute_import
 
 import json
 
+from mgr_module import CLICommand
+from .. import mgr
+
 
 class CmdException(Exception):
     def __init__(self, retcode, message):
@@ -13,6 +16,16 @@ class CmdException(Exception):
 def exec_dashboard_cmd(command_handler, cmd, **kwargs):
     cmd_dict = {'prefix': 'dashboard {}'.format(cmd)}
     cmd_dict.update(kwargs)
+    for cli_cmd in CLICommand.COMMAND_LIST:
+        if cmd_dict['prefix'] == cli_cmd.prefix:
+            ret, out, err = cli_cmd.call(mgr, cmd_dict, None)
+            if ret < 0:
+                raise CmdException(ret, err)
+            try:
+                return json.loads(out)
+            except ValueError:
+                return out
+
     ret, out, err = command_handler(cmd_dict)
     if ret < 0:
         raise CmdException(ret, err)
